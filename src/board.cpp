5e51358802789069680d4b7c364142bb509b6197
pol-sito <polestomar@gmail.com>
2026-03-18 14:29:09 +0100

    ShouldExplode hecho
    ShouldExplode con el contador que va contando hacia las dos direcciones de cada eje hecho

--- src/board.cpp
+++ src/board.cpp
@@ -65,6 +65,126 @@ int Board::getHeight() const
 bool Board::shouldExplode(int x, int y) const
 {
     // Implement your code here
+
+    Candy* origen = getCell(x, y); //si el caramelo es caramelo vacio salimos
+    if (origen == nullptr) {
+        return false;
+    }
+
+    CandyType tipoOrigen = origen->getType(); //guardamos el tipo del caramelo
+    int contador = 1; //no contar el del centro
+    int compX = 0;
+    int compY = 0;
+    Candy* vecino = nullptr;
+
+    //Eje X
+
+    //izq
+    compX = x - 1;
+    vecino = getCell(compX, y);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++; //va comprobando si son del mismo tipo hacia la izquierda
+        compX--;
+        vecino = getCell(compX, y);
+    }
+
+    //der
+    compX = x + 1;
+    vecino = getCell(compX, y);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++; //va comprobando si son del mismo tipo hacia la derecha
+        compX++;
+        vecino = getCell(compX, y);
+    }
+
+    //todos los del mismo tipo se guardan en contador para saber si deberían explotar
+
+    if (contador >= SHORTEST_EXPLOSION_LINE) {
+        return true;
+    }
+
+    //Eje Y
+    contador = 1;
+
+    //arriba
+    compY = y - 1;
+    vecino = getCell(x, compY);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++;
+        compY--;
+        vecino = getCell(x, compY);
+    }
+
+    //abajo
+    compY = y + 1;
+    vecino = getCell(x, compY);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++;
+        compY++;
+        vecino = getCell(x, compY);
+    }
+
+    if (contador >= SHORTEST_EXPLOSION_LINE) {
+        return true;
+    }
+
+    //Eje ArribaIzq -> AbajoDer
+    contador = 1;
+
+    //ArribaIzq
+    compX = x - 1;
+    compY = y - 1;
+    vecino = getCell(compX, compY);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++;
+        compX--;
+        compY--;
+        vecino = getCell(compX, compY);
+    }
+
+    //AbajoDer
+    compX = x + 1;
+    compY = y + 1;
+    vecino = getCell(compX, compY);
+    while (vecino != nullptr and vecino->getType() == tipoOrigen) {
+        contador++;
+        compX++;
+        compY++;
+        vecino = getCell(compX, compY);
+    }
+
+    if (contador >= SHORTEST_EXPLOSION_LINE) {
+        return true;
+    }
+
+    //Eje AbajoIzq -> ArribaDer
+    contador = 1;
+
+    //AbajoIzq
+    compX = x - 1;
+    compY = y + 1;
+    vecino = getCell(compX, compY);
+    while (vecino != nullptr && vecino->getType() == tipoOrigen) {
+        contador++;
+        compX--; compY++;
+        vecino = getCell(compX, compY);
+    }
+
+    //ArribaDer
+    compX = x + 1;
+    compY = y - 1;
+    vecino = getCell(compX, compY);
+    while (vecino != nullptr && vecino->getType() == tipoOrigen) {
+        contador++;
+        compX++; compY--;
+        vecino = getCell(compX, compY);
+    }
+
+    if (contador >= SHORTEST_EXPLOSION_LINE) {
+        return true;
+    }
+
+    //Si el contador no ha llegado minimo hasta 3 en todos los ejes significa que no tiene que explotar
     return false;
 }
 
