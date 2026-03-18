2c5f7e107d71a11efa56860b2f7a00ac64535e42
AdriFdez <fdtadrian@gmail.com>
2026-03-18 14:01:52 +0100

    Función dump y tipoAString

--- src/board.cpp
+++ src/board.cpp
@@ -1,6 +1,8 @@
 #include "board.h"
 #include <memory>
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 Board::Board(int width, int height)
 {
@@ -72,10 +74,50 @@ std::vector<Candy*> Board::explodeAndDrop()
     return {};
 }
 
+// NUEVA FUNCION: Devuelve un string con el tipo de caramelo 
+// para que sea mas intuitiva la lectura del archivo de guardado.
+std::string tipoAString(CandyType type)
+{
+    switch (type)
+    {
+    case CandyType::TYPE_BLUE:   return "BLUE";
+    case CandyType::TYPE_GREEN:  return "GREEN";
+    case CandyType::TYPE_ORANGE: return "ORANGE";
+    case CandyType::TYPE_PURPLE: return "PURPLE";
+    case CandyType::TYPE_RED:    return "RED";
+    case CandyType::TYPE_YELLOW: return "YELLOW";
+    default:                     return "UNKNOWN";
+    }
+}
+
 bool Board::dump(const std::string& output_path) const
 {
     // Implement your code here
-    return false;
+    ofstream boardSave(output_path);
+    // Solo se ejecuta si se ha podido abrir bien el archivo.
+    if (boardSave)
+    {
+        for (int c = 0; c < boardX; c++)
+        {
+            for (int f = 0; f < boardY; f++)
+            {
+                Candy* caramelo = getCell(c, f);
+
+                // Si hay caramelo en la celda [c,f] entonces guarda
+                if (caramelo != nullptr)
+                {
+                    boardSave << tipoAString(caramelo->getType()) << " " << c << " " << f << endl;
+                }
+            }
+        }
+        boardSave.close();
+        return true;
+    }
+    else
+    {
+        cout << "Error de apertura del archivo." << endl;
+        return false;
+    }
 }
 
 bool Board::load(const std::string& input_path)
